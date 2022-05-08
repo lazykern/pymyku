a156686bbedf61ad40b114858aa5c54d4a2882f5
phusitsom <phusitsom@gmail.com>
2022-05-08 18:39:02 +0700

    Add classifiers

--- setup.py
+++ setup.py
@@ -4,6 +4,7 @@ setup(
     name='pymyku',
     version='0.1.0',
     description='Python MyKU API Wrapper',
+    long_description=open('README.md').read(),
     author='phusitsom',
     author_email='phusitsom@gmail.com',
     packages=['pymyku'],
@@ -11,4 +12,15 @@ setup(
     install_requires=['requests'],
     license='GNU General Public License v3.0',
     python_requires='>=3.6',
+    classifiers=[
+        'Natural Language :: English',
+        'License :: OSI Approved :: GNU General Public License v3.0',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+    ]
 )
