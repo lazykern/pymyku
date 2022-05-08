29877b1006b4e03c9f8a91c6f49023a777a027f2
phusitsom <phusitsom@gmail.com>
2022-05-08 16:28:23 +0700

    Added schedule attributes

--- pymyku/__init__.py
+++ pymyku/__init__.py
@@ -2,6 +2,7 @@ from .attribute import FetchedResponses as FetchedResponsesAttr
 from .attribute import Student as StudentAttr
 from .attribute import Token as TokenAttr
 from .attribute import User as UserAttr
+from .attribute import Schedule as ScheduleAttr
 from .constant import APP_KEY
 from .exception import InvalidSubjectID, TokenExpired
 from .pymyku import Client
