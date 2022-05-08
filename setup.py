4aa2d47a7c14b15b48591512aa1726dec63f2994
phusitsom <phusitsom@gmail.com>
2022-05-08 11:28:50 +0700

    Created setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='pymyku',
+    version='0.1',
+    description='Python MyKU API Wrapper',
+    author='phusitsom',
+    author_email='phusitsom@gmail.com',
+    packages=['pymyku'],
+    url='https://github.com/phusitsom/pymyku',
+    install_requires=['aenum', 'requests'],
+    license='GNU General Public License v3.0'
+)
