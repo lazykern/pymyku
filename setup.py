c028a471a1687afca705db2cd11e046ee548dfaa
phusitsom <phusitsom@gmail.com>
2022-05-08 17:04:02 +0700

    Change version form 0.1 to 0.1.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='pymyku',
-    version='0.1',
+    version='0.1.0',
     description='Python MyKU API Wrapper',
     author='phusitsom',
     author_email='phusitsom@gmail.com',
