79b0aa415124f65f6e0abaaa4ff99a58d7e4ed90
phusitsom <phusitsom@gmail.com>
2022-05-09 18:56:29 +0700

    Fix invalid path

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ extra_requires = {
     ],
 }
 version = ''
-with open('./pymyku.__init__.py') as f:
+with open('./pymyku/__init__.py') as f:
     version = re.search(r'^__version__\s*=\s*[\'"]([^\'"]*)[\'"]', f.read(), re.MULTILINE).group(1)
 
 setup(
