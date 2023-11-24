2037b38f7f29034d035f7d0565fdd52fcaa89538
Phusit Somboonyingsuk <phusitsom@gmail.com>
2023-11-24 20:06:05 +0700

    Make getschedule params optional

--- pymyku/api/common/__init__.py
+++ pymyku/api/common/__init__.py
@@ -6,11 +6,11 @@ from pymyku.utils import amend_locals, raise_for_status_with_response
 
 def getschedule(
     session: Session,
-    std_status_code: int,
-    campus_code: str,
-    faculty_code: str,
-    major_code: str,
-    user_type: str
+    std_status_code: str = '',
+    campus_code: str = '',
+    faculty_code: str = '',
+    major_code: str = '',
+    user_type: str = '',
 ) -> Getschedule:
 
     res = session.get(
