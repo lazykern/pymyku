2ae78b2b8e35a29663efe39e94187729333efeca
Phusit Somboonyingsuk <phusitsom@gmail.com>
2023-11-24 20:06:42 +0700

    Allow client creating only via credentials

--- pymyku/pymyku.py
+++ pymyku/pymyku.py
@@ -4,7 +4,7 @@ from dataclass_wizard import JSONWizard
 from requests import Session
 
 from pymyku import __version__, consts, utils
-from pymyku.api.auth import login
+from pymyku import api
 
 
 @dataclass()
@@ -25,38 +25,25 @@ class Payload(JSONWizard):
 
 
 class PyMyKU:
-    def __init__(self):
+    def __init__(self, username: str, password: str):
         self.__session: Session = Session()
-        self.__payload: Payload | None = None
+        self.__access_token_payload: Payload
 
         self.__session.headers.update({
            "User-Agent": f"pymyku/{__version__}",
            "app-key": consts.APP_KEY,
         })
 
-    @staticmethod
-    def from_credentials(username: str, password: str) -> PyMyKU:
-        client = PyMyKU()
 
         hashed_username = utils.hash_credential(username)
         hashed_password = utils.hash_credential(password)
 
-        login_res = login(client.session, hashed_username, hashed_password)
+        self.__login_res = api.auth.login(self.__session, hashed_username, hashed_password)
 
-        client.set_access_token(login_res.accesstoken)
-
-        return client
-
-    @staticmethod
-    def from_access_token(access_token: str) -> PyMyKU:
-        client = PyMyKU()
-
-        client.set_access_token(access_token)
-
-        return client
+        self.set_access_token(self.__login_res.accesstoken)
 
     def set_access_token(self, access_token: str) -> None:
-        self.__payload = Payload.from_dict(
+        self.__access_token_payload = Payload.from_dict(
             utils.decode_access_token(access_token)
         )
 
@@ -69,5 +56,33 @@ class PyMyKU:
         return self.__session
 
     @property
-    def payload(self):
-        return self.__payload
+    def access_token_payload(self):
+        return self.__access_token_payload
+
+    @property
+    def user(self):
+        return self.__login_res.user
+
+    @property
+    def student(self):
+        return self.__login_res.user.student
+    
+    def get_schedule(self, std_status_code: str = '', campus_code: str = '', faculty_code: str = '', major_code: str = '', user_type: str = '') -> api.common.Getschedule:
+
+        if std_status_code == '':
+            std_status_code = self.student.student_status_code
+
+        if campus_code == '':
+            campus_code = self.student.campus_code
+
+        if faculty_code == '':
+            faculty_code = self.student.faculty_code
+
+        if major_code == '':
+            major_code = self.student.major_code
+
+        if user_type == '':
+            user_type = self.__login_res.user.user_type
+
+        return api.common.getschedule(self.session, std_status_code, campus_code, faculty_code, major_code, user_type)
+    
